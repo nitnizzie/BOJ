c207937be637229f7214b36cb688c3e21d0ec1da
vonnish <shinhyuk1001@kaist.ac.kr>
2022-01-15 21:43:37 +0900

    add run.sh

---
+++ run.sh
@@ -0,0 +1,12 @@
+if [ $1 ]; then
+    if [ -d "/$1" ]; then
+        g++ -o $1/$1.out $1/$1.cpp
+        ./$1/$1.out
+    else
+        echo "create new file on /$1"
+        mkdir $1
+        touch $1/$1.cpp
+    fi
+else
+    echo "usage: ./run.sh <num>"
+fi
