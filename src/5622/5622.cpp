cafb7ce627e8c27bcc39b1698d183329cfa60dcf
vonnish <shinhyuk1001@kaist.ac.kr>
2022-02-08 15:55:38 +0900

    5622 solution with c++ updated

---
+++ src/5622/5622.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string code;
+    int sum = 0;
+    cin >> code;
+
+    for (int i = 0; i < code.length(); i++) {
+        sum += ((int)code[i]-65)/3 + 3;
+
+        if (code[i] == 'S' || code[i] == 'V' || code[i] == 'Y' || code[i] == 'Z' ) sum--;
+    }
+
+    cout << sum;
+}
